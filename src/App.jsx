ca123e91faaa09e46a7730a493ab4fdccc78947e
christopher goodwin <goodwin.chris2021@gmail.com>
2023-11-22 20:41:39 -0600

    initial commit, fixed weird esbuild bug

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+// import Hero from "./components/Hero";
+// import Demo from "./components/Demo";
+
+import "./App.css";
+
+const App = () => {
+    return <div className='text-orange-600 font-satoshi text-5xl'>App</div>;
+};
+
+export default App;
