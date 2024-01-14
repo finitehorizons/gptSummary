280c0487b0fe5e734b5870dce94c47f702d1280a
Christopher Goodwin <goodwin.chris2021@gmail.com>
2024-01-13 23:08:13 -0600

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,19 +4,32 @@ const Hero = () => {
     return (
         <header className='w-full flex justify-center items-center flex-col'>
             <nav className='flex justify-between items-center w-full mb-10 pt-3'>
-                <img src={logo} alt='sumz-logo' className='w-48 object-contain' />
-                <button type='button' onClick={() => window.open("https://github.com/finitehorizons")} className='black_btn'>
+                <img
+                    src={logo}
+                    alt='sumz-logo'
+                    className='w-48 object-contain'
+                />
+                <button
+                    type='button'
+                    onClick={() =>
+                        window.open("https://github.com/finitehorizons")
+                    }
+                    className='black_btn'>
                     Github
                 </button>
             </nav>
 
             <h1 className='head_text'>
-                Summarize Articles Using  <br className='max-md:hidden' />
+                Summarize Articles Using <br className='max-md:hidden' />
                 <span className='orange_gradient'>OpenAI GPT-4</span>
             </h1>
             <h2 className='desc letter-spacing_balance'>
-                Unlock the potential of articles effortlessly with <strong>SummarizeFlow</strong>, a cutting-edge SaaS tool integrating GPT-4 for intelligent summarization. Dive into the heart of any piece with ease with clear, digestible summaries.
-                Revolutionize your reading experience and save valuable time with this intuitive and powerful tool.
+                Unlock the potential of articles effortlessly with{" "}
+                <strong>SummarEase</strong>, a cutting-edge SaaS tool
+                integrating GPT-4 for intelligent summarization. Dive into the
+                heart of any piece with ease with clear, digestible summaries.
+                Revolutionize your reading experience and save valuable time
+                with this intuitive and powerful tool.
             </h2>
         </header>
     );
