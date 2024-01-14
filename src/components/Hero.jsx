1d93ad2187fa414ba73822cd1dca4622620890cc
Christopher Goodwin <goodwin.chris2021@gmail.com>
2024-01-13 22:09:24 -0600

    updated Logo
    updated Logo image and some text.

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,14 +4,14 @@ const Hero = () => {
     return (
         <header className='w-full flex justify-center items-center flex-col'>
             <nav className='flex justify-between items-center w-full mb-10 pt-3'>
-                <img src={logo} alt='sumz-logo' className='w-28 object-contain' />
+                <img src={logo} alt='sumz-logo' className='w-48 object-contain' />
                 <button type='button' onClick={() => window.open("https://github.com/finitehorizons")} className='black_btn'>
                     Github
                 </button>
             </nav>
 
             <h1 className='head_text'>
-                Summarize Articles with <br className='max-md:hidden' />
+                Summarize Articles Using  <br className='max-md:hidden' />
                 <span className='orange_gradient'>OpenAI GPT-4</span>
             </h1>
             <h2 className='desc letter-spacing_balance'>
